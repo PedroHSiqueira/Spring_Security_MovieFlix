43741696fa848f23c9d3b6cc9dea48f923859073
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2026-03-07 16:17:55 -0300

    Feat: Filtro JWT

--- src/main/java/com/movieflix/config/SecurityConfig.java
+++ src/main/java/com/movieflix/config/SecurityConfig.java
@@ -11,12 +11,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    private final SecurityFilter securityFilter;
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
